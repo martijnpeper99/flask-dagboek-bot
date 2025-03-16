7a2e7f9ce417b3d68eb56563cbc8314e5944eafd
MP010 <martijnpeper99@hotmail.com>
2025-03-16 14:50:48 +0100

    Fix Flask app import

--- Procfile
+++ Procfile
@@ -1,3 +1,4 @@
-web: gunicorn -w 4 -b 0.0.0.0:8000 app:app
+web: gunicorn -w 4 -b 0.0.0.0:8000 --log-level=debug app2:app
+
 
 
--- app2.py
+++ app2.py
@@ -174,4 +174,4 @@ def generate_diary():
 
 # **Server starten**
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8000)
